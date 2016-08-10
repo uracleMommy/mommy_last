4321e3d031a8b1612e402a4ff5d1108f49d86b30
ocs1209 <fatesuan@fatesuan.local>
2016-08-10 15:36:00 +0900

    commit
    commit

---
+++ Controller/SignUpMain.h
@@ -0,0 +1,15 @@
+//
+//  SignUpMain.h
+//  co.medisolution
+//
+//  Created by uracle on 2016. 8. 10..
+//  Copyright © 2016년 medisolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpMain : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ Controller/SignUpMain.m
@@ -0,0 +1,39 @@
+//
+//  SignUpMain.m
+//  co.medisolution
+//
+//  Created by uracle on 2016. 8. 10..
+//  Copyright © 2016년 medisolution. All rights reserved.
+//
+
+#import "SignUpMain.h"
+
+@interface SignUpMain ()
+
+@end
+
+@implementation SignUpMain
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    _scrollView.contentSize = CGSizeMake(320, 1000);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
